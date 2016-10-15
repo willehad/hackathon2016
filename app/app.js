b55ccf9b03f730ce12336889e44450faeae36a7c
willehad <willehad@gmail.com>
2016-10-15 19:45:15 +0200

    new app.js

--- app/app.js
+++ app/app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(express.static('public'));
+
+app.get('/rest/hello', function (req, res) {
+  res.json({reply: 'Hello World!'});
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Starting app);
 });
 
