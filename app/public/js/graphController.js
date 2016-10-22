5986d9de8692d7a0057b783a9a05ae303173e68c
Conny Andersson <connyandersson88@gmail.com>
2016-10-22 12:00:52 +0200

    made blobs into objects to make them easier to manipulate

--- app/public/js/graphController.js
+++ app/public/js/graphController.js
@@ -1,21 +1,31 @@
+var fbBlob = {x:250, y:200, rad:40, color:"#4E59D4", name:"FB"};
+var twitterBlob = {x:770, y:200, rad:40, color:"#1da1f2", name:"TWTR"};
+var googleBlob = {x:250, y:400, rad:40, color:"#ea4335", name:"GOOG"};
+var linkedinBlob = {x:770, y:400, rad:40, color:"#0077b5", name:"LNKD"};
 
-function drawMainNode(x, y, r, color, symbol)
+
+function drawMainNode(blob)
 {
 	var c = document.getElementById("graphCanvas");
 	var ctx = c.getContext("2d");
 	ctx.beginPath();
-	ctx.arc(x,y,r,0,2*Math.PI);
-	ctx.fillStyle = color;
+	ctx.arc(blob.x,blob.y,blob.rad,0,2*Math.PI);
+	ctx.fillStyle = blob.color;
 	ctx.fill();
 	ctx.beginPath();
 	ctx.font = "25px Arial";
 	ctx.fillStyle = "black";
 	ctx.textAlign = 'center';
-	ctx.fillText(symbol,x, y+10);
+	ctx.fillText(blob.name,blob.x, blob.y+10);
 	ctx.fill();
 	ctx.stroke();
 }
-drawMainNode(250,200,40,"#4E59D4", "FB");
-drawMainNode(770,200,40,"#1da1f2", "TWTR");
-drawMainNode(250,400,40,"#ea4335", "GOOG");
-drawMainNode(770,400,40,"#0077b5", "LNKD");
+
+drawMainNode(fbBlob);
+drawMainNode(twitterBlob);
+drawMainNode(googleBlob);
+drawMainNode(linkedinBlob);
+//drawMainNode(250,200,40,"#4E59D4", "FB");
+//drawMainNode(770,200,40,"#1da1f2", "TWTR");
+//drawMainNode(250,400,40,"#ea4335", "GOOG");
+//drawMainNode(770,400,40,"#0077b5", "LNKD");
